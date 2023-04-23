2be3492eac15117e56ac4207fbf38756a6635661
bprigent <hello@bprigent.com>
2023-04-23 16:17:23 -0400

    connected the two files

--- logic.js
+++ logic.js
@@ -1,2 +1,12 @@
 //file dedicated to generating a random message
 
+
+// get array from other file
+const quotes_js_file = require('./quotes.js');
+let quotes_arr = quotes_js_file.exported_quotes_arr;
+
+//counting the number of quotes automatically
+const quotes_number = quotes_arr.length;
+
+console.log(quotes_arr);
+console.log(quotes_number);
--- quotes.js
+++ quotes.js
@@ -1,13 +1,14 @@
 //list of quotes
-quotes_arr = [
+let quotes_arr = [
     "This is a first quote",
     "This is another quote",
     "Lorem upsum quote number 3",
-    "final quote, quote number 4"
+    "final quote, quote number 4",
+    "another line is good to make it 5"
 ];
 
-//counting the number of quotes automatically
-const quotes_number = quotes_arr.length;
+//export the array to other files
+exports.exported_quotes_arr = quotes_arr;
 
 //testing if quotes_number works
 //console.log(quotes_number);
