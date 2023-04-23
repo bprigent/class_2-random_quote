aa6897eae2d2ec696d6e84ce53475c0d6c31288a
bprigent <hello@bprigent.com>
2023-04-23 16:32:02 -0400

    html added, js modified

--- logic.js
+++ logic.js
@@ -5,20 +5,22 @@
 
 // get array from other file
 const quotes_js_file = require('./quotes.js');
-let quotes_arr = quotes_js_file.exported_quotes_arr;
+const quotes_arr = quotes_js_file.exported_quotes_arr;
 
 //counting the number of quotes automatically
 const quotes_number = quotes_arr.length;
 
-// generate a randome number between 0 and the quotes arr length
-let random_num = Math.floor(Math.random() * quotes_number);
+// function to generate randome quote
+function generate_random_quote() {
+    // generate a randome number between 0 and the quotes arr length
+    let random_num = Math.floor(Math.random() * quotes_number);
 
-// get associated random quote from random number
-let random_quote = quotes_arr[random_num]
+    // get associated random quote from random number
+    let random_quote = quotes_arr[random_num];
+    
+    return random_quote;
+}
 
 
-//tests
-console.log(quotes_arr);
-console.log(quotes_number);
-console.log(random_num);
-console.log(random_quote);
+//test 
+console.log(generate_random_quote());
