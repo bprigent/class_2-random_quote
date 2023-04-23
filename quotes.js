1a154d0ec7e7743e84bc74ac1defabdb63019795
bprigent <hello@bprigent.com>
2023-04-23 15:09:59 -0400

    first commit

---
+++ quotes.js
@@ -0,0 +1,13 @@
+//list of quotes
+quotes_arr = [
+    "This is a first quote",
+    "This is another quote",
+    "Lorem upsum quote number 3",
+    "final quote, quote number 4"
+];
+
+//counting the number of quotes automatically
+const quotes_number = quotes_arr.length;
+
+//testing if quotes_number works
+console.log(quotes_number);
