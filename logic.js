bac85693838a8c1450a91dd703cf217dd6ea636b
bprigent <hello@bprigent.com>
2023-04-24 11:24:35 -0400

    removed quotes js file

--- logic.js
+++ logic.js
@@ -1,22 +1,25 @@
 //file dedicated to generating a random message
 ////////////////////////////////
 ////////////////////////////////
-
+const quotes_arr = [
+    "This is a first quote",
+    "This is another quote",
+    "Lorem upsum quote number 3",
+    "final quote, quote number 4",
+    "another line is good to make it 5"
+];
 
 // function to generate randome quote
 function generate_random_quote() {
-    // get array from other file
-    const quotes_js_file = require('./quotes.js');
-    const quotes_arr_child = quotes_js_file.exported_quotes_arr;
-
+    
     //counting the number of quotes automatically
-    const quotes_number = quotes_arr_child.length;
+    const quotes_number = quotes_arr.length;
 
     // generate a randome number between 0 and the quotes arr length
     let random_num = Math.floor(Math.random() * quotes_number);
 
     // get associated random quote from random number
-    let random_quote = quotes_arr_child[random_num];
+    let random_quote = quotes_arr[random_num];
     
     console.log("hello");
     return random_quote;
